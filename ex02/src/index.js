f7e1b5c96a6014c636aa3088a997840718fd9119
DeniEdit <milicevic.deni@gmail.com>
2021-03-15 12:51:07 +0100

    Exercise 02

---
+++ ex02/src/index.js
@@ -0,0 +1,12 @@
+// Write code below this line
+function functionPrintInfo(name,age) {
+    console.log("John",23);
+        console.log("Michael", 43);
+}
+
+functionPrintInfo("John",23);
+functionPrintInfo("Michael", 43);
+
+// Write code below this line
+
+module.exports = functionPrintInfo;

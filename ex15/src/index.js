3a68c96af2a1ca2bb54f6b08bb9fc574e3f08d74
DeniEdit <milicevic.deni@gmail.com>
2021-03-15 18:18:07 +0100

    Exercise 15

---
+++ ex15/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+function convertToInt(str) {
+    var a = parseInt("BA");
+    var a = parseInt("F1");
+    var a = parseInt("JeffBezos");
+
+    console.log(str);
+}
+convertToInt(186);
+convertToInt(241);
+convertToInt(NaN);
+// Only change code below this line
+module.exports = convertToInt;

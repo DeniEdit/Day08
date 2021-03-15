16528b422050790f64d5fc89ff03c87c6ea8baae
DeniEdit <milicevic.deni@gmail.com>
2021-03-15 16:18:58 +0100

    Exercise 08

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+var total = 0;
+
+function funcWithArg(digit) {
+    (digit + 9) / 5;
+}
+
+funcWithArg(6);
+
+// Only change code below this line
+console.log(3);
+module.exports = funcWithArg;

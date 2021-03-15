a273eec3a764dce44265776d3f4c82968678cfab
DeniEdit <milicevic.deni@gmail.com>
2021-03-15 17:32:38 +0100

    Exercise 10

---
+++ ex10/src/index.js
@@ -0,0 +1,19 @@
+// Only change code below this line
+function mathTest(x, y) {
+    console.log(8);
+}
+
+mathTest(2, 2);
+// Only change code below this line
+if(x < 0 || y < 0) {
+// Only change code below this line
+console.log(8);
+} else {
+    console.log(undefined);
+// Only change code below this line
+}
+return Math.round(Math.pow(Math.sqrt(x) + Math.sqrt(y), 2));
+// Only change code below this line
+
+// Only change code below this line
+module.exports = mathTest;

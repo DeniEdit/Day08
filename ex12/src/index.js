ee904ec16b200c3faf170097d57325a69977f0fd
DeniEdit <milicevic.deni@gmail.com>
2021-03-15 17:51:06 +0100

    Exercise 12

---
+++ ex12/src/index.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+function randomInteger(){
+Math.floor(Math.random() * 30);
+} 
+randomInteger();
+// Only change code below this line
+module.exports = randomInteger;

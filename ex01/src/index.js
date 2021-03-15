403d20908c3aae1990a71734e1b104b3ce04d5b2
DeniEdit <milicevic.deni@gmail.com>
2021-03-15 12:35:26 +0100

    Functions

---
+++ ex01/src/index.js
@@ -0,0 +1,7 @@
+// Write code below this line
+function reusableFunction() {
+    console.log("Hi Padawan!");
+}
+// Write code below this line
+console.log(reusableFunction());
+module.exports = reusableFunction;

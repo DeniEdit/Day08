681a0397dc01e651436c589c622aab777f361471
DeniEdit <milicevic.deni@gmail.com>
2021-03-15 17:56:35 +0100

    Exercise 13

---
+++ ex13/src/index.js
@@ -0,0 +1,7 @@
+// Only change code below this line
+function randomRangeNumber(minNumber, maxNumber){
+Math.floor(Math.random() * (maxNumber - minNumber +1)) + min;
+} 
+randomRangeNumber();
+// Only change code below this line
+module.exports = randomRangeNumber;

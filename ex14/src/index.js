d197a0a684081cf633f11277ef110286722f71dd
DeniEdit <milicevic.deni@gmail.com>
2021-03-15 18:14:10 +0100

    Exercise 14

---
+++ ex14/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+function convertStrToInt(str) {
+   var a  = parseInt("44");
+   var a  = parseInt("27");
+   var a  = parseInt("JeffBezos");
+
+   console.log(str);
+}
+    convertStrToInt(44);
+    convertStrToInt(27);
+    convertStrToInt(NaN);
+    // Only change code below this line
+    module.exports = convertStrToInt;

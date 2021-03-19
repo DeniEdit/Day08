45faac82bcb36ea08bed0b810e445d58d4cefcd9
DeniEdit <milicevic.deni@gmail.com>
2021-03-19 14:24:39 +0100

    Exercise 05

--- ex05/src/index.js
+++ ex05/src/index.js
@@ -2,16 +2,11 @@
 var dress = "little dress";
 function name() {
 
-// Only change code below this line
 return dress;
 }
-
 function myDress () {
     var dress = "ball gown";
 }
-// Only change code below this line
-
-// Only change code below this line
+myDress(dress);
 console.log(dress);
-
 module.exports = myDress;

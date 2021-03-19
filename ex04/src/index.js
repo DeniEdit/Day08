9a148b535006f977878c4387601ebac1a6a3e88d
DeniEdit <milicevic.deni@gmail.com>
2021-03-19 14:15:11 +0100

    Exercise 04

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,18 +1,13 @@
-// Only change code below this line
 var myVariable;
-
 function localScope() {
     var myVariable = "I am here!";
     console.log(myVariable);
 }
-// Only change code below this line
 localScope();
 console.log(myVariable);
-// myVariable is not defined outside of localScope
 if(typeof myVariable != "undefine"){
     console.log('outside localScope', myVariable)
 } else {
     console.log('outside localScope UNDEFINED!!!')
 }
-
 module.exports = localScope;

cd94fbceed6ecec003f7f1f558f0133dd324758a
DeniEdit <milicevic.deni@gmail.com>
2021-03-19 14:31:21 +0100

    Exercise 07

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,18 +1,14 @@
-// Only change code below this line
 var sum = 0;
 function addThree() {
     sum = sum + 3;
     console.log("addThree: " + sum);
 }
 addThree();
-
 function addFive() {
     sum = sum + 5;
     console.log("addFive: " + sum);
 }
 addFive();
-
-// Only change code below this line
 module.exports = {
     addThree,
     addFive
